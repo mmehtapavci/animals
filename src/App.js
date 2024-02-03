fb07ee9b84d732bb32876d638767115294452b64
mmehtapavci <mmmehtapavci@gmail.com>
2024-02-03 23:49:17 +0300

    comment lines have been added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { useState } from "react";
 
+// Bu fonksiyonun görevi ise tanımladığımız listeden rasgele hayvan seçer.
 function getRandomAnimal() {
     const animals = ['bird', 'cat', 'cow', 'dog', ' gator', 'horse'];
 
     return animals[Math.floor(Math.random() * animals.length)];
 }
 
-
+// Bu fonksiyonun görevi tıklandığında rasgele seçilen hayvan listesine yenisini ekler ve son gündel durumu verir bize 
 function App() {
     const [animals, setAnimals] = useState([]);
     const handleClick = () => {
         setAnimals([...animals, getRandomAnimal()]);
     };
 
-
+    // handleClick'e her tıklandığında animals döndürür
     return <div>
         <button onClick={handleClick}>Add Animal! </button>
         <div>{animals}</div>
