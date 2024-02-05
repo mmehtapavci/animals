668f12479a20ece03928ca947a4b3073b0f826c0
mmehtapavci <mmmehtapavci@gmail.com>
2024-02-05 16:53:29 +0300

    Animal img add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
 
-// Bu fonksiyonun görevi ise tanımladığımız listeden rasgele hayvan seçer.
 function getRandomAnimal() {
-    const animals = ['bird', 'cat', 'cow', 'dog', ' gator', 'horse'];
-
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
     return animals[Math.floor(Math.random() * animals.length)];
 }
 
-// Bu fonksiyonun görevi tıklandığında rasgele seçilen hayvan listesine yenisini ekler ve son gündel durumu verir bize 
 function App() {
     const [animals, setAnimals] = useState([]);
     const handleClick = () => {
         setAnimals([...animals, getRandomAnimal()]);
     };
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />;
+    });
 
-    // handleClick'e her tıklandığında animals döndürür
-    return <div>
-        <button onClick={handleClick}>Add Animal! </button>
-        <div>{animals}</div>
-    </div>
-
+    return (
+        <div>
+            <button onClick={handleClick}>Add Animal! </button>
+            <div>{renderedAnimals}</div>
+        </div>
+    );
 }
 
 export default App;
